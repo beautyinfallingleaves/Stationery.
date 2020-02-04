b04ec2706f0c775c2c92d9f59fa7507113e9a548
Chris Kang <christopha.kang@gmail.com>
2020-02-03 20:56:57 -0600

    cleanup firebase config

---
+++ constants/index.js
@@ -0,0 +1,17 @@
+// ***********************************
+// ** FIREBASE CONFIG, NEEDED FOR STORAGE OF IMAGES.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGQXUmAaRusFPjOHtEZNMLqs7dbHRfE5Q",
+  authDomain: "stationery-75d14.firebaseapp.com",
+  databaseURL: "https://stationery-75d14.firebaseio.com",
+  projectId: "stationery-75d14",
+  storageBucket: "stationery-75d14.appspot.com",
+  messagingSenderId: "1034649031130",
+  appId: "1:1034649031130:web:b01b212685e0d391da30e3",
+  measurementId: "G-NXV504NXPY"
+}
+
+// ***********************************
+// ** SERVER ADDRESS FOR MAKING API REQUESTS.
+// ** DEFAULTS TO THE HEROKU HOSTING BUT CAN BE CHANGED FOR LOCAL DEV.
+export const serverUrl = 'https://stationery-post.herokuapp.com'
